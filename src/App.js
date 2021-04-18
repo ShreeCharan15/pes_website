da6acc975c34fbd6bbb10ec77d50e3d94a1ac0ae
ShreeCharan15 <shreecharan150718@gmail.com>
2021-04-18 18:44:04 +0530

    land page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import { Box, Center } from '@chakra-ui/layout';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Box bgGradient="linear(to-br, white, #9dea7e)" style={{minHeight:"100vh"}}>
+      <Box w="100%" h="70px"   borderRadius="0px 0px 40px 40px"  padding={3}>
+          <div className="container">
+            <div className="row">
+              <div className="col-auto">
+                <img src="./logo-blue.png" style={{maxHeight:"60px"}}></img>
+              </div>
+              <div className="col">
+              </div>
+              <div className="col">
+              </div>
+              
+            </div>
+          </div>
+      </Box>
+      <br/>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-6" >
+          <Center style={{height:"100%"}}>
+          <img src="./logo.png" style={{maxHeight:"450px",minHeight:"250px"}}></img>
+          </Center>
+          </div>
+          <div className="col-12 col-md-6">
+            <Box borderWidth={1} borderRadius="lg" padding={5} shadow="2xl" borderColor="#9dea7e">
+            <div className="display-4" style={{color:"#234E52",fontSize:"30px",fontWeight:"400"}}>PES Day</div>
+            On April 22, 2008, the Power Engineering Society changed its name to the Power & Energy Society to focus on helping the environment and the society. This day has been officially commemorated as “PES Day” for the past three years. All activities conducted in the month of April are counted towards PES Day celebration.
+          <br/><br/>
+          <div className="display-4" style={{color:"#234E52",fontSize:"30px",fontWeight:"400"}}>PES Day 2021 Theme</div>
+            Climate Change is perhaps one of the largest problem’s humanity has faced. Our world is changing faster than ever. Being the society that governs the engineering aspect of energy production that has resulted in greenhouse gasses and global warming, we must educate everyone on the solutions and technological developments that are happening to slow down this issue.
+            <p  style={{color:"#234E52",fontWeight:"500",margin:"10px 0px"}}>The theme of the 4th IEEE PES DAY is “Clean Energy Revolution”.</p>
+            <p style={{margin:"10px 0px"}}>Considering the advisory given by IEEE and WHO, keeping in mind the health of the people, 
+            <span style={{color:"#234E52",fontWeight:"500"}}> PES Day 2021 is going to be a strictly virtual event.</span>
+            </p>
+            <p style={{margin:"10px 0px"}}>
+            With this theme in mind, let’s organize virtual events by connecting with a global network of IEEE Members, PES Volunteers, PES Young Professionals to improve PES Day Technical, Humanitarian, Women in Power, and Young Professional Activities.
+            </p>
+            <p style={{color:"#234E52",fontWeight:"500",margin:"10px 0px"}}>We would like to Encourage our IEEE Members across the Globe to help bring “More Power To the Future with Clean Energy Revolution.”
+            </p>
+            </Box>
+          </div>
+        </div>
+      </div>
+      </Box>
     </div>
   );
 }
